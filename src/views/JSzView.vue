79ea7c59f3c0a6dbb2132f840b328eb9fb37c0c1
Jacek Szpyrka <jszpyrka@gmail.com>
2025-07-18 12:28:46 +0200

    counter toi JSz page added

--- src/views/JSzView.vue
+++ src/views/JSzView.vue
@@ -1,12 +1,20 @@
+<script setup lang="ts">
+  import { useCounterStore } from '@/stores/counter';
+  const counter = useCounterStore();
+</script>
+
+
 <template>
-  <div class="about">
+  <div class="jsz">
     <h1>This is an JSz page</h1>
+    <h2>Licznik: {{ counter.count }}</h2>
+    <button @click="counter.increment">Kliknij mnie</button>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .jsz {
     min-height: 100vh;
     display: flex;
     align-items: center;
